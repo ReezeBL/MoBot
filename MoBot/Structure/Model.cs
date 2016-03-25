87b46bd02626330c1cdbc379b147e72d4efb6bb4
ReezeBL <shilk.e82@gmail.com>
2016-03-25 15:08:29 +0300

    base commit

---
+++ MoBot/Structure/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoBot.Structure
+{
+    class Model : IObservable<SysAction>
+    {
+        public IDisposable Subscribe(IObserver<SysAction> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MoBot/Structure/Viewer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoBot.Structure
+{
+    partial class Viewer : Form, IObserver<SysAction>
+    {
+        Controller mainController;
+        public Viewer()
+        {
+            InitializeComponent();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(SysAction value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
